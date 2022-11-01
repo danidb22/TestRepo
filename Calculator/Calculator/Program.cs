f6c66b164890591b25fbcd2c1746653e11d52911
danidb22 <dannyabebe3@gmail.com>
2022-11-01 18:55:16 -0400

    initial commit

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
